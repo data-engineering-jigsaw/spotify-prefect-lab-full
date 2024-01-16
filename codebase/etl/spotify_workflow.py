5af5dbe6d5e092df0b75f9d2b40aa0942436720c
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-01-16 14:34:51 -0500

    update adapter

--- codebase/etl/spotify_workflow.py
+++ codebase/etl/spotify_workflow.py
@@ -8,18 +8,17 @@ def get_playlist_tracks(playlist_id):
 
 @task
 def extract_tracks_info(tracks, playlist_id):
-    return adapter.extract_tracks_info(tracks, playlist_id)
+    pass
 
 @task
 def write_to_csv(tracks):
-    return adapter.write_to_csv(tracks)
+    pass
 
 @flow
 def extract_and_write():
     playlist_id = "37i9dQZEVXbLRQDuF5jeBp"
     playlist_tracks = get_playlist_tracks(playlist_id)
     # fill in remainder of flow here
-    selected_tracks = extract_tracks_info(playlist_tracks, playlist_id)
-    write_to_csv(selected_tracks)
+    
 
 extract_and_write()
